8082adbc3b436e4df7ccc1700e64cb69a0824aab
Kathryn-z <71240245+Kathryn-z@users.noreply.github.com>
2020-10-27 19:03:50 +0800

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 let balls = [];
 
 function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < 10; i++) {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 20; i++) {
     balls[i] = new Ball();
   }
 }
 
 function draw() {
-  background(200);
+  background(255);
   for (let ball of balls) {
     ball.update();
     ball.display();
@@ -19,19 +19,25 @@ class Ball {
   constructor() {
     this.x = width / 2;
     this.y = random(height);
-    this.dy = random(-1,1);
+    this.dy = random(-20,20);
+    this.radius=random(20,50);
+    this.color1 = random(100,200);
+    this.color2 = random(100,200);
+    this.color3 = random(100,200);
   }
   update() {
     this.x += this.dy;
-    if (this.x - 30 <= 0) {
+    if (this.x - 15 <= 0) {
       this.dy = -this.dy;
     }
-    if (this.x + 30 >= width) {
+    if (this.x + 15 >= width) {
       this.dy = -this.dy;
     }
   }
   display() {
-    circle(this.x, this.y, 30);
+    noStroke();
+    fill(this.color1,this.color2,this.color3)
+    circle(this.x, this.y, this.radius);
   }
 }
 
